2b9cbd164fc0bd95d06712edfcf918e74bb8925e
Coin de Gamma <cg@corpglory.com>
2021-06-27 11:49:09 +0200

    ball

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,34 @@ use macroquad::prelude::*;
 
 type Point = (u16, u16);
 
-struct Bar {
-    length: u16,
-    offset: u16
+struct Ball {
+    position: Point
 }
 
+impl Ball {
+    fn render(&self, game: &Game) {
+        game.render_pixel(self.position, WHITE);
+    }
+}
+
+
+struct Bar {
+    length: u16,
+    offset: u16,
+    x: u16
+}
+
+impl Bar {
+    fn render(&self, game: &Game) {
+        let from = self.offset;
+        let to = self.offset + self.length;
+        for i in from..to {
+            game.render_pixel((self.x, i), WHITE);
+        }
+    }
+}
+
+        
 struct Game {
     pixel_width: u16,
     pixel_heigh: u16,
@@ -15,39 +38,59 @@ struct Game {
     game_screen_height: u16,
     game_width: u16,
     game_height: u16,
-    player_left: Bar,
-    player_right: Bar
+    bar_left: Bar,
+    bar_right: Bar,
+    ball: Ball
 }
 
 impl Game {
     fn new() -> Game {
 
-        let player_left = Bar {
-            length: 10,
-            offset: 5
+        let pixel_width = 14;
+        let pixel_heigh = 14;
+        let game_screen_width = 658;
+        let game_screen_height = 406;
+        let game_width = 658 / 14;
+        let game_height = 406 / 14;
+
+        let bar_length = 10;
+        let bar_offset = 8;
+
+        let bar_left = Bar { 
+            length: bar_length, 
+            offset: bar_offset,
+            x: 0
         };
 
-        let player_right = Bar {
-            length: 10,
-            offset: 5
+        let bar_right = Bar { 
+            length: bar_length, 
+            offset: bar_offset,
+            x: game_width - 1
         };
+
+        let ball = Ball {
+            position: (game_width / 2, game_height / 2)
+        };
+        
 
         return Game {
-            pixel_width: 14,
-            pixel_heigh: 14,
-            game_screen_width: 658,
-            game_screen_height: 406,
-            game_width: 658 / 14,
-            game_height: 406 / 14,
-            player_left,
-            player_right
+            pixel_width,
+            pixel_heigh,
+            game_screen_width,
+            game_screen_height,
+            game_width,
+            game_height,
+            bar_left,
+            bar_right,
+            ball
         }
     }
 
     fn render(&self) {
         clear_background(BLACK);
-        self.render_middle_border();
+        self.render_borders();
         self.render_bars();
+        self.ball.render(&self);
     }
 
     fn render_pixel(&self, p: Point, color: Color) {
@@ -58,22 +101,26 @@ impl Game {
         draw_rectangle(x, y, pw, ph, color);
     }
 
-    fn render_middle_border(&self) {
+    fn render_borders(&self) {
         let middle = self.game_width / 2;
         for i in 0..self.game_height {
             if i % 2 == 0 {
                 self.render_pixel((middle, i), GRAY);
             }
         }
+        draw_line(
+            0., 0., screen_width() , 0., 
+            self.pixel_heigh as f32, GRAY
+        );
+        draw_line(
+            0., screen_height(), screen_width() , screen_height(), 
+            self.pixel_heigh as f32, GRAY
+        );
     }
 
     fn render_bars(&self) {
-        for i in self.player_left.offset..self.player_left.length {
-            self.render_pixel((0, i), WHITE);
-        }
-        for i in self.player_right.offset..self.player_left.length {
-            self.render_pixel((self.game_width - 1, i), WHITE);
-        }
+        self.bar_left.render(&self);
+        self.bar_right.render(&self);
     }
 }
 
